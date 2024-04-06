1b9e9d24f9cbf63be5c4ecf6bd8fab22fde28055
princerodrick59 <116687818+princerodrick59@users.noreply.github.com>
2024-04-05 22:58:05 -0500

    Added Adv Kit logging to current code - Ready to merge to main

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-BSide";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "2dd8d9f184bd9b214afc50d0b8afdbb38dd4834e";
-  public static final String GIT_DATE = "2024-04-02 21:14:10 CDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-04-03 20:48:12 CDT";
-  public static final long BUILD_UNIX_TIME = 1712195292771L;
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "30182ff13ab8aa7aa5b7ee16a153f686e5060f64";
+  public static final String GIT_DATE = "2024-04-05 22:55:41 EDT";
+  public static final String GIT_BRANCH = "Adv-Kit-Logging";
+  public static final String BUILD_DATE = "2024-04-05 23:55:39 EDT";
+  public static final long BUILD_UNIX_TIME = 1712375739452L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
